c454579afe7b7ff2fa0dfa04d6a3733941447217
Felix Torto <fatorto76@gmail.com>
2025-02-14 12:50:45 -0500

    Assignment Submission

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: _FELIX A TORTO_ Student ID: 168365229  Date: __2025_02_14_
+*
+********************************************************************************/ 
+
+
 /*var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
